fb3fcbc837bf452a8d7c21df96cb1b9862b5cd45
Otvhque <142947931+Otvhque@users.noreply.github.com>
2025-08-27 14:18:33 -0300

    Refiz as 2 atividades que havia perdido

---
+++ html/js/exer8.js
@@ -0,0 +1,8 @@
+n1 = parseInt(prompt("Informe a primeira nota:\n"))
+n2 = parseInt(prompt("Informe a segunda nota:\n"))
+n3 = parseInt(prompt("Informe a terceira nota:\n"))
+n4 = parseInt(prompt("Informe a quarta nota:\n"))
+
+r = (n1+n2+n3+n4)/4
+
+alert("Sua média anual foi de: "+r)
---
+++ html/js/exer9.js
@@ -0,0 +1,6 @@
+n1 = parseFloat(prompt("Informe o valor da compra:\n"))
+
+r1 = n1*1.15
+r2 = n1*0.85
+
+alert("O valor da sua compra com 15% de acréscimo é: "+r1+"\n\nE o valor com 15% de desconto é:" +r2)
